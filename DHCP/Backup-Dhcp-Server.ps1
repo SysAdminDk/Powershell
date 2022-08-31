dd23038538c3a516691ab5248e300b633feb91e9
Jan Kristensen <96570455+SysAdminDk@users.noreply.github.com>
2022-08-31 14:35:06 +0200

    Update Backup-Dhcp-Server.ps1

--- DHCP/Backup-Dhcp-Server.ps1
+++ DHCP/Backup-Dhcp-Server.ps1
@@ -2,15 +2,29 @@
     .DESCRIPTION
     Download and import selected security baselines from Microsoft Security Compliance Toolkit
 
-    .PARAMETER DownloadID
+    .PARAMETER Path 
+    Specifies where the backup file will be created.
+    This can be local path or a UNC path.
     
     .EXAMPLE
     .\Backup-Dhcp-Server.ps1 -Path "\\FILE01\Backup\DHCP"
 
 #>
 
-# --------------------------------------------------------------------------------------------------
+#
+# Create folder if missing.
+#
+if ((!Test-Path -Path $Path)) {
+    New-Item -Path $Path -ItemType Directory -Force | Out-Null
+}
+
+#
 # Backup DHCP server
-# --------------------------------------------------------------------------------------------------
-$FileName = (get-date).ToString('dd-MM-yyyy')
-Export-DhcpServer -Leases -File "$UNCPath\DHCP-Backup-$FileName.xml"
+#
+Try {
+    $FileName = (get-date).ToString('dd-MM-yyyy')
+    Export-DhcpServer -Leases -File "$Path\DHCP-$FileName.xml"
+} Catch {
+    Write-Output "Unable to backup DHCP server"
+    Write-Output $_
+}
