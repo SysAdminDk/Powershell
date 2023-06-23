99f0313613f435854e09d3a7ceeb922df8adea90
Jan Kristensen <96570455+SysAdminDk@users.noreply.github.com>
2023-06-23 11:19:42 +0200

    Create Disable-Stale-Computers.ps1

---
+++ Active Directory/Disable-Stale-Computers.ps1
@@ -0,0 +1,14 @@
+<#
+    .DESCRIPTION
+     Find and disable Inactive Computers
+
+#>
+
+$LastLogon = (Get-Date).Adddays(-90)
+$CurrentDate = Get-Date -Format "dd-MM-yyyy"
+
+$Computers = Get-ADComputer -Filter { LastLogonTimeStamp -lt $LastLogon -and Enabled -eq 'True' }  -Properties Description
+Foreach ($Computer in $Computers) {
+    Disable-ADAccount -Identity $Computer.DistinguishedName
+    Set-ADComputer -Identity $Computer.DistinguishedName -Description $("[Disabled, $CurrentDate] $($Computer.Description)")
+}
