ea102224126946d1df28bf7ecf7c4a2ea24e1bb5
Jan Kristensen <96570455+SysAdminDk@users.noreply.github.com>
2023-06-23 11:56:58 +0200

    Update Disable-Stale-Users.ps1

--- Active Directory/Disable-Stale-Users.ps1
+++ Active Directory/Disable-Stale-Users.ps1
@@ -9,6 +9,8 @@ $CurrentDate = Get-Date -Format "dd-MM-yyyy"
 
 $Users = Get-ADUser -Filter { LastLogonTimeStamp -lt $LastLogon -and Enabled -eq 'True' } -Properties Description
 Foreach ($User in $Users) {
+    Write-Verbose "Disable User : $($User.DistinguishedName)"
+
     Disable-ADAccount -Identity $User.DistinguishedName
     Set-AdUser -Identity $User.DistinguishedName -Description $("[Disabled, $CurrentDate] $($User.Description)")
 }
