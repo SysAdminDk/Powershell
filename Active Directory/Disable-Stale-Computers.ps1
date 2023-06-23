8db6f70c9800c6638689b321ffed9633590a9044
Jan Kristensen <96570455+SysAdminDk@users.noreply.github.com>
2023-06-23 11:56:16 +0200

    Update Disable-Stale-Computers.ps1

--- Active Directory/Disable-Stale-Computers.ps1
+++ Active Directory/Disable-Stale-Computers.ps1
@@ -9,6 +9,8 @@ $CurrentDate = Get-Date -Format "dd-MM-yyyy"
 
 $Computers = Get-ADComputer -Filter { LastLogonTimeStamp -lt $LastLogon -and Enabled -eq 'True' }  -Properties Description
 Foreach ($Computer in $Computers) {
+    Write-Verbose "Disable Computer : $($Computer.DistinguishedName)"
+
     Disable-ADAccount -Identity $Computer.DistinguishedName
     Set-ADComputer -Identity $Computer.DistinguishedName -Description $("[Disabled, $CurrentDate] $($Computer.Description)")
 }
