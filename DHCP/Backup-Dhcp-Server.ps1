c09e999be829eb1181f211370aed022fe55937bd
Jan Kristensen <96570455+SysAdminDk@users.noreply.github.com>
2023-07-24 10:05:13 +0200

    Update Backup-Dhcp-Server.ps1

--- DHCP/Backup-Dhcp-Server.ps1
+++ DHCP/Backup-Dhcp-Server.ps1
@@ -1,18 +1,22 @@
 <#
     .DESCRIPTION
-    Create backup of DHCP scope and leases.
+    Create a backup of DHCP scope and leases.
 
     .PARAMETER Path 
     Specifies where the backup file will be created.
-    This can be local path or a UNC path.
+    This can be a local path or a UNC path, but the user running the script must have write permissions to the path.
     
     .EXAMPLE
     .\Backup-Dhcp-Server.ps1 -Path "\\FILE01\Backup\DHCP"
 
 #>
+[CmdletBinding()]
+Param(
+  [Parameter(ValueFromPipelineByPropertyName=$true,Position=0)][string]$Path
+)
 
 #
-# Create folder if missing.
+# Create the folder if missing.
 #
 if ((!Test-Path -Path $Path)) {
     New-Item -Path $Path -ItemType Directory -Force | Out-Null
