67f1670925978641afc0cb3077943db27dc01db4
Jan Kristensen <96570455+SysAdminDk@users.noreply.github.com>
2024-05-07 08:27:56 +0200

    Update Setup-Dhcp-Server.ps1
    DISCLAIMER

--- DHCP/Setup-Dhcp-Server.ps1
+++ DHCP/Setup-Dhcp-Server.ps1
@@ -1,14 +1,11 @@
 <#
-
-    DISCLAIMER:
+    .DISCLAIMER
 
     THE SCRIPT IS PROVIDED AS-IS, WITHOUT WARRANTY OF ANY KIND. USE AT YOUR OWN RISK.
 
     By running this script, you acknowledge that you have read and understood the disclaimer, and you agree to assume
     all responsibility for any failures, damages, or issues that may arise as a result of executing this script.
 
-#>
-<#
     .DESCRIPTION
     Install required features for DHCP services and Restore scopes and leases, to latest backup file found on the supplied Path
 
