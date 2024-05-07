bea7ddfa88bf579aee8412e7fe0da625a009a6ea
Jan Kristensen <96570455+SysAdminDk@users.noreply.github.com>
2024-05-07 08:25:16 +0200

    Update Set-SelfPermissions.ps1
    DISCLAIMER

--- LAPS/Set-SelfPermissions.ps1
+++ LAPS/Set-SelfPermissions.ps1
@@ -1,5 +1,16 @@
 <#
 
+    DISCLAIMER:
+
+    THE SCRIPT IS PROVIDED AS-IS, WITHOUT WARRANTY OF ANY KIND. USE AT YOUR OWN RISK.
+
+    By running this script, you acknowledge that you have read and understood the disclaimer, and you agree to assume
+    all responsibility for any failures, damages, or issues that may arise as a result of executing this script.
+
+#>
+
+<#
+
  Search Current Domain for all Computers, extract OUs names and select unique OUs
  Select the OUs where computers needs permission to write the Windows AND Legacy LAPS password
 
