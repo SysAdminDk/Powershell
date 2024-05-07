bf35876bde5de91c78fad15fc8893087d2dfc389
Jan Kristensen <96570455+SysAdminDk@users.noreply.github.com>
2024-05-07 08:24:40 +0200

    Update Setup-ActiveDirectroy-Laps.ps1

--- LAPS/Setup-ActiveDirectroy-Laps.ps1
+++ LAPS/Setup-ActiveDirectroy-Laps.ps1
@@ -1,4 +1,14 @@
 <#
+
+    DISCLAIMER:
+
+    THE POWERSHELL SCRIPT IS PROVIDED AS-IS, WITHOUT WARRANTY OF ANY KIND. USE AT YOUR OWN RISK.
+
+    By running this script, you acknowledge that you have read and understood the disclaimer, and you agree to assume
+    all responsibility for any failures, damages, or issues that may arise as a result of executing this script.
+
+#>
+<#
     .SYNOPSIS
      Extend Active Directory to support both Legacy LAPS and Windows LAPS
     
