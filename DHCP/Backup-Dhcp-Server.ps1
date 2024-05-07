c1c23fa458a977462b14f9a63a337e10efa65ce2
Jan Kristensen <96570455+SysAdminDk@users.noreply.github.com>
2024-05-07 08:26:26 +0200

    Update Backup-Dhcp-Server.ps1
    DISCLAIMER

--- DHCP/Backup-Dhcp-Server.ps1
+++ DHCP/Backup-Dhcp-Server.ps1
@@ -1,4 +1,14 @@
 <#
+
+    DISCLAIMER:
+
+    THE SCRIPT IS PROVIDED AS-IS, WITHOUT WARRANTY OF ANY KIND. USE AT YOUR OWN RISK.
+
+    By running this script, you acknowledge that you have read and understood the disclaimer, and you agree to assume
+    all responsibility for any failures, damages, or issues that may arise as a result of executing this script.
+
+#>
+<#
     .DESCRIPTION
     Create a backup of DHCP scope and leases.
 
