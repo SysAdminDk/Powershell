c56a0254f1d085e0e51a6899fb86ae16b0fd2e2e
Jan Kristensen <96570455+SysAdminDk@users.noreply.github.com>
2024-06-20 09:49:07 +0200

    Update Inventory-ScheduledTasks.ps1

--- Active Directory/Inventory/Inventory-ScheduledTasks.ps1
+++ Active Directory/Inventory/Inventory-ScheduledTasks.ps1
@@ -2,26 +2,27 @@
     Script to Inventory Scheduled Tasks on all servers
 
     Excludes tasks thats running as "System", "Network Service" and "local Service"
-    - If all tasks are to be listed change $ExcludeAccounts variable at line 37
+    - If all tasks are to be listed change $ExcludeAccounts variable at line 19
 
     Outputs to screen, no CSV or log.
 
 #>
-# ----------------------------------------------------------------------------------------------------
+
 # List all Windows Servers in the domain, excluding Domain Controllers (There shouldn't be any)
 # ----------------------------------------------------------------------------------------------------
 $AllServers = Get-ADComputer -Filter "operatingSystem -like 'Windows Server*'"
 $AllServers = $AllServers | Where {$_.DistinguishedName -notlike "*Domain*"}
 
+
+# Exclude from inventory
+# ----------------------------------------------------------------------------------------------------
+$ExcludeAccounts = @($null,"System","Network Service", "local Service")
+
+
 Foreach ($Server in $AllServers) {
 
-    # Verify RDP connection.
-    $NetPing = Test-NetConnection -ComputerName $Server.DNSHostName -CommonTCPPort RDP
-    if ($NetPing.TcpTestSucceeded) {
-        Write-Output "$($Server.DNSHostName) : RDP ok"
-    }
-
     # Test connection with WinRM (needed for remote PowerShell)
+    # ----------------------------------------------------------------------------------------------------
     $NetWinRM = Test-NetConnection -ComputerName $Server.DNSHostName -CommonTCPPort WINRM
     If (!($NetWinRM.TcpTestSucceeded)) {
         Write-Warning "Unable to connect to $($Server.DNSHostName)"
@@ -31,6 +32,7 @@ Foreach ($Server in $AllServers) {
         Write-Output "Connect WinRM"
 
         # Connect and get all shcduled tasks
+        # ----------------------------------------------------------------------------------------------------
         Invoke-Command -ComputerName $Server.DNSHostName -Authentication NegotiateWithImplicitCredential -ScriptBlock {
 
             $Command = Get-Command -Name "Get-ScheduledTask" -ErrorAction SilentlyContinue
@@ -39,8 +41,7 @@ Foreach ($Server in $AllServers) {
                 Write-Warning "$($ENV:Computername) : The PowerShell command `"Get-ScheduledTask`" is not recognized"
 
             } else {
-                $ExcludeAccounts = @($null,"System","Network Service", "local Service")
-                $Tasks = Get-ScheduledTask | Select-Object TaskName, State, @{Name="RunAs";Expression={ $_.principal.userid }} | Where {$_.RunAs -notin $ExcludeAccounts}
+                $Tasks = Get-ScheduledTask | Select-Object TaskName, State, @{Name="RunAs";Expression={ $_.principal.userid }} | Where {$_.RunAs -notin $Using:ExcludeAccounts}
                 if ($Tasks) {
                     Write-Output $Tasks
                 }
